045d828f5aaf88c27bfb159741fe5b35fbda98b7
salehhesham244 <saleh.sharouk02@eng-st.cu.edu.eg>
2023-07-14 15:22:29 -0700

    sah

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-{	  
+{
 printf("%d is positive\n", n);
 }
 else if (n == 0)
@@ -24,6 +24,6 @@ else
 {
 printf("%d is negative\n", n);
 }
-return 0;
+return (0);
 }
 
